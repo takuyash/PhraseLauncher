5748ca356b215734e143aabd6aee0a4fca94e6c8
takuya <14888534+takuyash@users.noreply.github.com>
2026-01-23 22:35:04 +0900

    Ctrl 2回とCtrl + Shift + Oで起動するように修正

--- PhraseLauncher/HiddenForm.cs
+++ PhraseLauncher/HiddenForm.cs
@@ -1,31 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using Timer = System.Windows.Forms.Timer;
 
 namespace PhraseLauncher
 {
-
-
     class HiddenForm : Form
     {
         Timer timer = new() { Interval = 50 };
         HashSet<Keys> pressed = new();
 
+        // --- Hook Fields ---
+        private static IntPtr _hookID = IntPtr.Zero;
+        private NativeMethods.LowLevelKeyboardProc _proc;
+        private DateTime _lastCtrlTime = DateTime.MinValue;
+        private const int DOUBLE_PRESS_MS = 300;
+        private bool _isCtrlPressed = false;
+
         public HiddenForm()
         {
+            _proc = HookCallback;
+
             Load += (s, e) =>
+            {
                 NativeMethods.RegisterHotKey(
                     Handle,
                     NativeMethods.HOTKEY_ID,
-                    NativeMethods.MOD_CONTROL_SHIFT,
-                    NativeMethods.VK_SPACE);
+                    NativeMethods.MOD_CONTROL | NativeMethods.MOD_SHIFT,
+                    NativeMethods.VK_O);
+
+                _hookID = SetHook(_proc);
+            };
 
             FormClosing += (s, e) =>
+            {
                 NativeMethods.UnregisterHotKey(Handle, NativeMethods.HOTKEY_ID);
+                NativeMethods.UnhookWindowsHookEx(_hookID);
+            };
 
             timer.Tick += Tick;
             timer.Start();
+        }
+
+        private IntPtr SetHook(NativeMethods.LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return NativeMethods.SetWindowsHookEx(NativeMethods.WH_KEYBOARD_LL, proc,
+                    NativeMethods.GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                // UP: 押し下げ状態解除
+                if (wParam == (IntPtr)NativeMethods.WM_KEYUP || wParam == (IntPtr)NativeMethods.WM_SYSKEYUP)
+                {
+                    if (vkCode == NativeMethods.VK_LCONTROL || vkCode == NativeMethods.VK_RCONTROL)
+                    {
+                        _isCtrlPressed = false;
+                    }
+                }
+
+                // DOWN: 判定
+                if (wParam == (IntPtr)NativeMethods.WM_KEYDOWN || wParam == (IntPtr)NativeMethods.WM_SYSKEYDOWN)
+                {
+                    if (vkCode == NativeMethods.VK_LCONTROL || vkCode == NativeMethods.VK_RCONTROL)
+                    {
+                        if (!_isCtrlPressed)
+                        {
+                            _isCtrlPressed = true;
+                            var now = DateTime.Now;
+                            if ((now - _lastCtrlTime).TotalMilliseconds <= DOUBLE_PRESS_MS)
+                            {
+                                JsonListForm.Show();
+                                _lastCtrlTime = DateTime.MinValue;
+                            }
+                            else
+                            {
+                                _lastCtrlTime = now;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        _lastCtrlTime = DateTime.MinValue;
+                    }
+                }
+            }
+            return NativeMethods.CallNextHookEx(_hookID, nCode, wParam, lParam);
         }
 
         protected override void WndProc(ref Message m)
@@ -71,5 +141,4 @@ namespace PhraseLauncher
             }
         }
     }
-
 }
--- PhraseLauncher/NativeMethods.cs
+++ PhraseLauncher/NativeMethods.cs
@@ -5,9 +5,19 @@ using System.Windows.Forms;
 static class NativeMethods
 {
     public const int HOTKEY_ID = 9001;
-    public const uint MOD_CONTROL_SHIFT = 0x0002 | 0x0004;
-    public const uint VK_SPACE = 0x20;
+    public const uint MOD_CONTROL = 0x0002;
+    public const uint MOD_SHIFT = 0x0004;
+    public const uint VK_O = 0x4F; // 'O' key
     public const int WM_HOTKEY = 0x0312;
+
+    // --- Hook Definitions ---
+    public const int WH_KEYBOARD_LL = 13;
+    public const int WM_KEYDOWN = 0x0100;
+    public const int WM_KEYUP = 0x0101;
+    public const int WM_SYSKEYDOWN = 0x0104;
+    public const int WM_SYSKEYUP = 0x0105;
+    public const int VK_LCONTROL = 0xA2;
+    public const int VK_RCONTROL = 0xA3;
 
     [DllImport("user32.dll")]
     public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
@@ -17,4 +27,19 @@ static class NativeMethods
 
     [DllImport("user32.dll")]
     public static extern short GetAsyncKeyState(Keys vKey);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    public static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
 }
